746d41d4e6cbdd6b40afb9a6fe30d8e2ca665d38
Panghui-L <384762718@qq.com>
2016-06-03 22:03:32 +0800

    homework1 2-3

---
+++ Main1.java
@@ -0,0 +1,18 @@
+package com.orilore.lxh.test1;
+
+public class Main1 extends Role {
+
+	public void fight(){
+		
+		System.out.println("Ä§·¨¹¥»÷");
+		
+		
+	}
+	
+	public void cure(){
+		
+		System.out.println("Ä§·¨ÖÎÁÆ");
+		
+	}
+	
+}
