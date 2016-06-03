0de9a8251a1b9b997dd93df6516c5185296276f1
Panghui-L <384762718@qq.com>
2016-06-03 22:07:23 +0800

    homework1 2-7

---
+++ showBlood.java
@@ -0,0 +1,26 @@
+package com.orilore.lxh.test1;
+
+public class showBlood{
+	
+Role role = new Role();
+
+public static void showBlood(SwordsMan swordsMan) {
+	
+	
+	SwordsMan role = null;
+	System.out.printf("%s 血量 %的，%n", role.getName(),role.getBlood());
+
+		
+		
+}
+public static void showBlood(Magician magician) {
+	
+	
+	SwordsMan role = null;
+	System.out.printf("%s 血量 %的，%n", role.getName(),role.getBlood());
+
+		
+		
+}
+
+}
